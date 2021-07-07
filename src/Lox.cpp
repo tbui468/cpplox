eef9e5fb5514c73ec223d28155ddb55c8316e7f4
Thomas Bui <thomasthienbui@gmail.com>
2021-07-07 20:40:09 +0700

    added optional arguments

--- src/Lox.cpp
+++ src/Lox.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
-int main()
+void runFile(char* script)
 {
-  std::cout << "Hello world";
+  std::cout << "running file: " << script << std::endl;
+}
+
+void runPrompt()
+{
+  std::cout << "running prompt" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+  if(argc > 2)
+  {
+    std::cout << "Usage: lox [script]" << std::endl;
+    return 1;
+  }
+  else if(argc == 2)
+  {
+    runFile(argv[1]);
+  }
+  else
+  {
+    runPrompt();
+  }
   return 0;
 }
