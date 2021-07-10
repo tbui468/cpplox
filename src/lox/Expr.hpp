e33e7aa299bdd5d787bc75ed6b61586fe40a1f70
Thomas Bui <thomasthienbui@gmail.com>
2021-07-10 15:41:47 +0700

    added template to Expr.hpp to test before modify GenAST tools

--- src/lox/Expr.hpp
+++ src/lox/Expr.hpp
@@ -1,38 +1,88 @@
 #ifndef EXPR_H
 #define EXPR_H
+
 #include "Token.hpp"
 namespace lox {
 
 struct Expr {
 };
 
+struct Binary;
+struct Grouping;
+struct Literal;
+struct Unary;
 
+template <typename T>
+struct VisitorInterface {
+  virtual T visit(Binary& e) = 0;
+  virtual T visit(Grouping& e) = 0;
+  virtual T visit(Literal& e) = 0;
+  virtual T visit(Unary& e) = 0;
+};
 
 struct Binary: public Expr {
-  Binary(Expr left, Token oprtr, Expr right): left(left), oprtr(oprtr), right(right) {}
-  Expr left;
-  Token oprtr;
-  Expr right;
+  public:
+    Binary(Expr left, Token oprtr, Expr right): left(left), oprtr(oprtr), right(right) {}
+    template <typename T>
+    T accept(VisitorInterface<T>& v) {
+      return v.visit(*this); 
+    }
+  public:
+    Expr left;
+    Token oprtr;
+    Expr right;
 };
-
 
 struct Grouping: public Expr {
-  Grouping(Expr expression): expression(expression) {}
-  Expr expression;
+  public:
+    Grouping(Expr expression): expression(expression) {}
+    template <typename T>
+    T accept(VisitorInterface<T>& v) {
+      return v.visit(*this); 
+    }
+  public:
+    Expr expression;
 };
-
 
 struct Literal: public Expr {
-  Literal(std::string value): value(value) {}
-  std::string value;
+  public:
+    Literal(std::string value): value(value) {}
+    template <typename T>
+    T accept(VisitorInterface<T>& v) {
+      return v.visit(*this); 
+    }
+  public:
+    std::string value;
 };
-
 
 struct Unary: public Expr {
-  Unary(Token oprtr, Expr right): oprtr(oprtr), right(right) {}
-  Token oprtr;
-  Expr right;
+  public:
+    Unary(Token oprtr, Expr right): oprtr(oprtr), right(right) {}
+    template <typename T>
+    T accept(VisitorInterface<T>& v) {
+      return v.visit(*this); 
+    }
+  public:
+    Token oprtr;
+    Expr right;
 };
+
+//implement version of this where T is std::string
+struct VisitorString: public VisitorInterface<std::string> {
+  std::string visit(Binary& e) override {
+    return "I'm the binary";
+  }
+  std::string visit(Grouping& e) override {
+    return "I'm the grouping";
+  }
+  std::string visit(Literal& e) override {
+    return "I'm the literal";
+  }
+  std::string visit(Unary& e) override {
+    return "I'm the unary";
+  }
+};
+
 }
 
 #endif //EXPR_H
